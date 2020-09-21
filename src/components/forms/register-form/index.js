c3d5eb4333d95a8efc0d6833d278b56b83bbca6d
hidanscript <dmarcano.ml@gmail.com>
2020-09-21 03:18:58 -0300

    IDK

--- src/components/forms/register-form/index.js
+++ src/components/forms/register-form/index.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { Card, Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { Formik } from 'formik';
-import yup from 'yup';
 import Axios from 'axios';
 import { SERVER_URL } from '../../../lib/endpoints';
 
@@ -10,10 +8,6 @@ function RegisterForm(props) {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
-
-  const schema = yup.object({
-    name: yup.string().required(),
-  });
 
   const registerUser = (event) => {
     event.preventDefault();
@@ -30,51 +24,49 @@ function RegisterForm(props) {
     <Card className="register-card">
       <Card.Header as="h5">Crea tu cuenta</Card.Header>
       <Card.Body>
-        <Formik validationSchema={schema}>
-          <Form onSubmit={registerUser}>
-            <Form.Group controlId="formName">
-              <Form.Label>Nombre</Form.Label>
-              <Form.Control 
-                type="text"
-                value={name}
-                onChange={(event) => setName(event.target.value)}
-                placeholder="Ingresa tu nombre"
-                isValid={name}
-                isInvalid={!name}
-              />
-            </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Correo</Form.Label>
-              <Form.Control 
-                type="email" 
-                value={email} 
-                onChange={(event) => setEmail(event.target.value)} 
-                placeholder="Ingresar correo"
-                isValid={email}
-                isInvalid={!email}
-              />
-            </Form.Group>
+        <Form onSubmit={registerUser}>
+          <Form.Group controlId="formName">
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control 
+              type="text"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+              placeholder="Ingresa tu nombre"
+              isValid={name}
+              isInvalid={!name}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Correo</Form.Label>
+            <Form.Control 
+              type="email" 
+              value={email} 
+              onChange={(event) => setEmail(event.target.value)} 
+              placeholder="Ingresar correo"
+              isValid={email}
+              isInvalid={!email}
+            />
+          </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Contraseña</Form.Label>
-              <Form.Control type="password" value={password} onChange={(event) => setPassword(event.target.value)} placeholder="Ingresar contraseña" />
-            </Form.Group>
-            <Button variant="primary flask-secondary-btn" block type="submit">
-              Registrarse
-            </Button>
-            <div className="separator"></div>
-            <Button variant="primary flask-facebook-btn" block type="submit">
-              <i className="text-white mr-2 fab fa-facebook-f"></i>
-              Registrarse con Facebook
-            </Button>
-            <Button variant="primary flask-google-btn" block type="submit">
-            <i className="text-white mr-2 fab fa-google"></i>
-              Registrarse con Google
-            </Button>
-            <div className="separator"></div>
-            <Form.Text className="text-center text-muted">¿Ya posees una cuenta?<br /><Link to="/login">Inicia sesión aquí</Link></Form.Text>
-          </Form>
-        </Formik>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control type="password" value={password} onChange={(event) => setPassword(event.target.value)} placeholder="Ingresar contraseña" />
+          </Form.Group>
+          <Button variant="primary flask-secondary-btn" block type="submit">
+            Registrarse
+          </Button>
+          <div className="separator"></div>
+          <Button variant="primary flask-facebook-btn" block type="submit">
+            <i className="text-white mr-2 fab fa-facebook-f"></i>
+            Registrarse con Facebook
+          </Button>
+          <Button variant="primary flask-google-btn" block type="submit">
+          <i className="text-white mr-2 fab fa-google"></i>
+            Registrarse con Google
+          </Button>
+          <div className="separator"></div>
+          <Form.Text className="text-center text-muted">¿Ya posees una cuenta?<br /><Link to="/login">Inicia sesión aquí</Link></Form.Text>
+        </Form>
       </Card.Body>
     </Card>
   );
